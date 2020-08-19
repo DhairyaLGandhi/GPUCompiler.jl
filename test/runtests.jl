a6e0d5b4f38d02df48b8451154de0c30da67669a
Tim Besard <tim.besard@gmail.com>
2020-08-19 16:00:03 +0200

    Move to Julia 1.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,6 @@ using GPUCompiler
 
 using LLVM, LLVM.Interop
 
-using Pkg
-
 include("util.jl")
 
 @testset "GPUCompiler" begin
@@ -17,10 +15,7 @@ GPUCompiler.enable_timings()
 
 include("native.jl")
 include("ptx.jl")
-if VERSION >= v"1.4"
-  Pkg.add(["SPIRV_LLVM_Translator_jll", "SPIRV_Tools_jll"])
-  include("spirv.jl")
-end
+include("spirv.jl")
 #include("gcn.jl")
 
 haskey(ENV, "CI") && GPUCompiler.timings()
